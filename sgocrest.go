f743da0624c4e7495b68143c09ef00d80b69f216
haryx8 <haryx8@yahoo.com>
2021-04-07 20:43:05 +0700

    Update file

--- sgocrest.go
+++ sgocrest.go
@@ -1,12 +1,15 @@
 package main
 
 import (
+	"fmt"
+	"image/jpeg"
 	"io"
 	"net/http"
 	"os"
 	"path/filepath"
 
 	"github.com/gabriel-vasile/mimetype"
+	"github.com/gen2brain/go-fitz"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
 	"github.com/otiai10/gosseract"
@@ -32,106 +35,130 @@ func main() {
 			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
 			`"bytes_out":${bytes_out}}` + "\n",
 	}))
-
 	e.GET("/", func(c echo.Context) error {
 		u := new(s)
-		u.Message = "hello bor!"
+		u.Message = "hello borld!"
 		return c.JSON(http.StatusOK, u)
 	})
-
-	e.POST("/read", ocr)
-
+	e.POST("/read/image", ocrImage)
+	e.POST("/read/file", ocrFile)
 	e.Logger.Fatal(e.Start(":1234"))
 }
 
-func ocr(c echo.Context) error {
+func ocrImage(c echo.Context) error {
 	// User ID from path `users/:id`
 	// id := c.Param("id")
 	// return c.String(http.StatusOK, "OK\n")
 
-	// Get
-	image, err := c.FormFile("image")
+	image, err := c.FormFile("data")
 	if err != nil {
 		return err
 	}
-
-	// Source
 	src, err := image.Open()
 	if err != nil {
 		return err
 	}
 	defer src.Close()
-
-	// Destination
 	uid := randstr.Hex(16)
-	// uuid := uuid.Must(uuid.NewV4())
-	// spew.Dump(uuid)
-
-	fn := "upload/" + uid + image.Filename
-
+	fn := filepath.Join("upload", "image")
+	fn = filepath.Join(fn, uid+image.Filename)
 	dst, err := os.Create(fn)
 	if err != nil {
 		return err
 	}
 	defer dst.Close()
-
-	// Copy
 	if _, err = io.Copy(dst, src); err != nil {
 		return err
 	}
-
 	u := new(r)
 	u.Message = ""
 	u.Data = ""
-
 	if fileExists(fn) {
-		allowed := []string{"image/jpeg", "image/png", "application/pdf"}
-		// allowed := []string{"image/jpeg", "image/png"}
+		allowed := []string{"image/jpeg", "image/png"}
 		mime, _ := mimetype.DetectFile(fn)
 		if mimetype.EqualsAny(mime.String(), allowed...) {
-			if mime.Extension() == ".pdf" {
-				u.Message = "Failed (pdf)"
-
-				// doc, err := fitz.New(fn)
-				// if err != nil {
-				// 	panic(err)
-				// }
-
-				// defer doc.Close()
-
-				// tmpDir, err := ioutil.TempDir(os.TempDir(), "fitz")
-				// if err != nil {
-				// 	panic(err)
-				// }
-
-				// // Extract pages as images
-				// for n := 0; n < doc.NumPage(); n++ {
-				// 	img, err := doc.Image(n)
-				// 	if err != nil {
-				// 		panic(err)
-				// 	}
-
-				// 	f, err := os.Create(filepath.Join(tmpDir, fmt.Sprintf("test%03d.jpg", n)))
-				// 	if err != nil {
-				// 		panic(err)
-				// 	}
-
-				// 	err = jpeg.Encode(f, img, &jpeg.Options{jpeg.DefaultQuality})
-				// 	if err != nil {
-				// 		panic(err)
-				// 	}
-
-				// 	f.Close()
-				// }
+			path := filepath.Join(fn)
+			im := gocv.IMRead(path, gocv.IMReadColor)
+			if im.Empty() {
+				u.Message = "Failed (Path)"
 			} else {
-				//run cv2 from python
-				// cmd := exec.Command("bash", "ocv.sh", fn)
-				// out, err := cmd.Output()
-				// if err != nil {
-				// u.Message = "Failed" + err.Error()
-				// }
+				gray := gocv.NewMat()
+				gocv.CvtColor(im, &gray, gocv.ColorBGRToGray)
+				blur := gocv.NewMat()
+				gocv.BilateralFilter(gray, &blur, 13, 15, 15)
+				scale := gocv.NewMat()
+				gocv.ConvertScaleAbs(blur, &scale, 1.5, 25)
+				gocv.IMWrite(fn, scale)
+				client := gosseract.NewClient()
+				client.SetImage(fn)
+				text, textErr := client.Text()
+				defer client.Close()
+				if textErr != nil {
+					u.Message = "Failed (Text)"
+					println(textErr.Error())
+				} else {
+					u.Data = text
+				}
+			}
+		} else {
+			os.Remove(fn)
+			u.Message = "Failed (Mime)"
+		}
+	} else {
+		u.Message = "Failed (File)"
+	}
+	return c.JSON(http.StatusOK, u)
+}
 
-				path := filepath.Join(fn)
+func ocrFile(c echo.Context) error {
+	file, err := c.FormFile("data")
+	if err != nil {
+		return err
+	}
+	src, err := file.Open()
+	if err != nil {
+		return err
+	}
+	defer src.Close()
+	uid := randstr.Hex(16)
+	fn := filepath.Join("upload", "pdf")
+	fn = filepath.Join(fn, uid+file.Filename)
+	dst, err := os.Create(fn)
+	if err != nil {
+		return err
+	}
+	defer dst.Close()
+	if _, err = io.Copy(dst, src); err != nil {
+		return err
+	}
+	u := new(r)
+	u.Message = ""
+	u.Data = ""
+	if fileExists(fn) {
+		allowed := []string{"application/pdf"}
+		mime, _ := mimetype.DetectFile(fn)
+		if mimetype.EqualsAny(mime.String(), allowed...) {
+			doc, err := fitz.New(fn)
+			if err != nil {
+				panic(err)
+			}
+			defer doc.Close()
+			for n := 0; n < doc.NumPage(); n++ {
+				img, err := doc.Image(n)
+				if err != nil {
+					panic(err)
+				}
+				path := filepath.Join("upload", "pdf", "image")
+				path = filepath.Join(path, fmt.Sprintf(uid+file.Filename+".%03d.jpg", n))
+				f, err := os.Create(path)
+				if err != nil {
+					panic(err)
+				}
+				err = jpeg.Encode(f, img, &jpeg.Options{jpeg.DefaultQuality})
+				if err != nil {
+					panic(err)
+				}
+				f.Close()
 				im := gocv.IMRead(path, gocv.IMReadColor)
 				if im.Empty() {
 					u.Message = "Failed (Path)"
@@ -142,14 +169,11 @@ func ocr(c echo.Context) error {
 					gocv.BilateralFilter(gray, &blur, 13, 15, 15)
 					scale := gocv.NewMat()
 					gocv.ConvertScaleAbs(blur, &scale, 1.5, 25)
-
-					gocv.IMWrite(fn, scale)
-
+					gocv.IMWrite(path, scale)
 					client := gosseract.NewClient()
-					client.SetImage(fn)
+					client.SetImage(path)
 					text, textErr := client.Text()
 					defer client.Close()
-
 					if textErr != nil {
 						u.Message = "Failed (Text)"
 						println(textErr.Error())
@@ -165,7 +189,6 @@ func ocr(c echo.Context) error {
 	} else {
 		u.Message = "Failed (File)"
 	}
-
 	return c.JSON(http.StatusOK, u)
 }
 
