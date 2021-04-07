bcf833c81f9d999f4747e53691131152e7865c9b
haryx8 <haryx8@yahoo.com>
2021-04-07 20:57:56 +0700

    Update file

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-go build sgocrest.go
+env GOOS=linux GOARCH=amd64 go build -v sgocrest.go
+# env GOOS=darwin GOARCH=amd64 go build -v sgocrest.go
+ls -lah|grep sgocrest
+upx sgocrest
+ls -lah|grep sgocrest
